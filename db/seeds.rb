a0977e45f333ef879c62a0c734d0a8e944fbae93
Ronald Stephens <rstephens@bluemorpho.cc>
2019-12-25 01:53:14 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-student = Student.create([{first_name: 'Rick'}, {last_name: 'Sanchez'}])
