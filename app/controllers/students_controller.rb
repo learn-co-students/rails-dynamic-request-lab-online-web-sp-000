c579248516664a5f7566e10edfdef759588f5519
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-05-08 10:37:28 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
+# warn_indent: true
+
 class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+    end
 end
